7ef505a536171fb4765af19fc5e4f860f812bad4
shykhabdull <mynameisshaikh20@gmail.com>
2024-08-01 08:55:29 +0500

    initial commit

---
+++ fullstackfyp/frontend/src/components/Footer.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  FaGithub ,
+  FaLinkedin ,
+  FaInstagram,
+  FaFacebook ,
+} from "react-icons/fa6";
+
+const Footer = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  return (
+    <>
+      <footer>
+        <div>
+        <a href="/">
+          <img src="/logo.png" alt="logo" />
+        </a>
+        </div>
+        <div>
+          <h4>Support Address</h4>
+          <ul>
+            <li>188, New Muslim Town {"\n"}  Abu Bakar Block  Garden Town, Lahore, Punjab 54500 </li>
+            <li>f20030055@orbit.edu.pk</li>
+            <li>+92 XXXXXXX </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li > <a href="/">
+              Home
+              </a>
+            </li>
+
+            <li > <a href="/jobs">
+              Jobs
+              </a>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+
+{/* Developer Credits */}
+
+<div >
+          <h4>Developer Credits</h4>
+          <ul>
+            <li>Shaikh Abdul Rehman (Team Lead)</li>
+            <li>Noman Khalid (Contributer)</li>
+            <li> Sharoon Naeem (Contributer) </li>
+          </ul>
+        </div>
+
+
+{/* Developer Credits */}
+
+        <div>
+          <h4>Contact Us </h4>
+          <ul>
+            <li>
+              <Link to={"https://github.com/ShykhAbdull"} target="_blank" rel="noopener noreferrer">
+                <span>
+                  <FaGithub />
+                </span>
+                <span>GitHub </span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"https://www.linkedin.com/in/shykh/"} target="_blank" rel="noopener noreferrer">
+                <span>
+                  <FaLinkedin  />
+                </span>
+                <span>LinkedIn</span>
+              </Link>
+            </li>
+            <li>
+            <Link to={"https://www.instagram.com/sh___y___kh/"} target="_blank" rel="noopener noreferrer">
+                <span>
+                  <FaInstagram />
+                </span>
+                <span>instagram</span>
+              </Link>
+            </li>
+            <li>
+            <Link to={"https://www.facebook.com/AbdullRocKs/"} target="_blank" rel="noopener noreferrer">
+                <span>
+                  <FaFacebook  />
+                </span>
+                <span>Facebook</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className="copyright">
+        &copy; 
+        CopyRight 2024. All Rights Reserved By Orbit Job Portal.
+      </div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ fullstackfyp/frontend/src/components/Hero.jsx
@@ -0,0 +1,73 @@
+
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { logout, clearAllUserErrors } from "../store/slices/userSlice";
+
+
+
+
+const Hero = () => {
+
+
+  const [show, setShow] = useState(false);
+  const [componentName, setComponentName] = useState("My Profile");
+
+  const { loading, isAuthenticated, error, user } = useSelector(
+    (state) => state.user
+  );
+
+  const dispatch = useDispatch();
+  const navigateTo = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    toast.success("Logged out successfully.");
+  };
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearAllUserErrors());
+    }
+    if (!isAuthenticated) {
+      navigateTo("/");
+    }
+
+  }, [dispatch, error, loading, isAuthenticated]);
+
+
+  return (
+    <section className="hero">
+
+
+<h1>
+  Hello! {user.name}
+</h1>
+      {user.role === "Employer" ? (
+        <h1>
+          <u style={{ textDecoration: 'underline', fontSize: 'inherit' }}>Post Jobs & review your applications</u>
+        </h1>
+      ) : user.role === "Job Seeker" ? (
+        <h1>
+          <u style={{ textDecoration: 'underline', fontSize: 'inherit' }}>Find & apply your Job today </u>
+        </h1>
+      ) : null}
+
+
+
+      <h4>
+        Connecting Talent with Opportunities Across the Nation for Every Skill
+        Level
+      </h4>
+      <div className="box">
+        Explore a vast array of job listings in diverse industries. Whether
+        you're a seasoned professional or just starting out, find the perfect
+        role to advance your career. Our platform makes job searching easy and
+        efficient, bringing you closer to your next big opportunity.
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
